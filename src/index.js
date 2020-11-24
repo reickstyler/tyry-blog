a7e298200c2ae285be67e63e66e61a117cefc13e
Tyler Reicks <tyler.reicks@gmail.com>
2020-11-23 23:15:45 -0700

    Adding nav bar and setting up Strapi.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import { ApolloProvider as ApolloHooksProvider } from "@apollo/react-hooks";
 import App from "./containers/App";
+import client from "./utils/apolloClient";
 import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <ApolloProvider client={client}>
+      <ApolloHooksProvider client={client}>
+        <App />
+      </ApolloHooksProvider>
+    </ApolloProvider>
   </Router>,
   document.getElementById("root")
 );
